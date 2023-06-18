84734f893808afca642521ae49ca259e7ecc7140
league <league@imac-24.attlocal.net>
2023-06-18 14:34:35 -0700

    first commit 2

--- src/league/GamePanel.java
+++ src/league/GamePanel.java
@@ -1,5 +1,25 @@
 package league;
 
-public class GamePanel {
+import java.awt.*;
 
+import javax.swing.*;
+
+public class GamePanel extends JPanel 
+{
+
+	
+	
+	
+	
+	
+	
+	@Override
+	public void paintComponent(Graphics g)
+	{
+		g.fillRect(10, 10, 100, 100);
+		
+
+	}
+
+	
 }
