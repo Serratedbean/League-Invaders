310f8b53ed61ade41e34f08c3921993314bec530
league <league@imac-24.attlocal.net>
2023-06-25 14:34:32 -0700

    le blue screen game has color now

--- src/league/GamePanel.java
+++ src/league/GamePanel.java
@@ -2,24 +2,70 @@ package league;
 
 import java.awt.*;
 
+import java.awt.Graphics;
+
 import javax.swing.*;
 
 public class GamePanel extends JPanel 
 {
 
+	final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
 	
+    int currentState = MENU;
 	
+    public void updateMenuState() 
+    {  
+    	
+    }
+    public void updateGameState() 
+    {  
+    	
+    }
+    public void updateEndState()  
+    {  
+    	
+    }
 	
-	
-	
-	
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	public void paintComponent(Graphics g)
 	{
-		g.fillRect(10, 10, 100, 100);
+		g.setColor(Color.black);
+		g.fillRect(100, 100, 100, 100);
+		
+		if(currentState == MENU){
+		    drawMenuState(g);
+		}
+		else if(currentState == GAME){
+		    drawGameState(g);
+		}
+		else if(currentState == END){
+		    drawEndState(g);
+		}
 		
 
 	}
+	
+	public void drawMenuState(Graphics g) 
+	{  
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+	public void  drawGameState(Graphics g) 
+	{  
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		
+	}
+	public void  drawEndState(Graphics g)  
+	{  
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+	}
+	
 
 	
 }
--- src/league/LeagueInvaders.java
+++ src/league/LeagueInvaders.java
@@ -2,9 +2,11 @@ package league;
 
 import javax.swing.JFrame;
 
+
 public class LeagueInvaders {
 	
 	JFrame frame = new JFrame();
+	GamePanel gamePanel = new GamePanel();
 
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 800;
@@ -21,6 +23,7 @@ public class LeagueInvaders {
 	
 	public void setup()
 	{
+		frame.add(gamePanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(WIDTH, HEIGHT);
 		frame.setVisible(true);
